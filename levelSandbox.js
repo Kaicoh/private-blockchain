34504841b8078aa89bae9d7f433065214744e8bc
Kaicoh <sumireminami@gmail.com>
2019-04-07 20:27:15 +0900

    modify db service to use promise

--- levelSandbox.js
+++ levelSandbox.js
@@ -9,31 +9,40 @@ const db = level(chainDB);
 
 // Add data to levelDB with key/value pair
 function addLevelDBData(key, value) {
-    db.put(key, value, function (err) {
-        if (err) return console.log(`Block ${key} submission failed`, err);
-        return null;
-    });
+    return db.put(key, value)
+        .then(() => true)
+        .catch((err) => {
+            console.log(`Block ${key} submission failed`, err);
+            return false;
+        });
 }
 
 // Get data from levelDB with key
 function getLevelDBData(key) {
-    db.get(key, function (err, value) {
-        if (err) return console.log('Not found!', err);
-        console.log(`Value = ${value}`);
-        return null;
-    });
+    return db.get(key)
+        .then((value) => {
+            console.log(`Value = ${value}`);
+            return value;
+        })
+        .catch((err) => {
+            console.log('Not found!', err);
+            return null;
+        });
 }
 
 // Add data to levelDB with value
 function addDataToLevelDB(value) {
     let i = 0;
-    db.createReadStream().on('data', function () {
-        i++;
-    }).on('error', function (err) {
-        return console.log('Unable to read data stream!', err);
-    }).on('close', function () {
-        console.log(`Block #${i}`);
-        addLevelDBData(i, value);
+    return new Promise((resolve, reject) => {
+        db.createReadStream().on('data', function () {
+            i += 1;
+        }).on('error', function (err) {
+            console.log('Unable to read data stream!', err);
+            reject(err);
+        }).on('close', function () {
+            console.log(`Block #${i}`);
+            resolve(addLevelDBData(i, value));
+        });
     });
 }
 
