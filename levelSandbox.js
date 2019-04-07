ee9628fec173de20a2a1133bb0198cc95ed65320
Kaicoh <sumireminami@gmail.com>
2019-04-07 19:44:40 +0900

    initialize repository

---
+++ levelSandbox.js
@@ -0,0 +1,59 @@
+/* ===== Persist data with LevelDB ===================================
+|  Learn more: level: https://github.com/Level/level     |
+|  =============================================================*/
+
+const level = require('level');
+
+const chainDB = './chaindata';
+const db = level(chainDB);
+
+// Add data to levelDB with key/value pair
+function addLevelDBData(key, value) {
+    db.put(key, value, function (err) {
+        if (err) return console.log(`Block ${key} submission failed`, err);
+        return null;
+    });
+}
+
+// Get data from levelDB with key
+function getLevelDBData(key) { // eslint-disable-line no-unused-vars
+    db.get(key, function (err, value) {
+        if (err) return console.log('Not found!', err);
+        console.log(`Value = ${value}`);
+        return null;
+    });
+}
+
+// Add data to levelDB with value
+function addDataToLevelDB(value) {
+    let i = 0;
+    db.createReadStream().on('data', function () {
+        i++;
+    }).on('error', function (err) {
+        return console.log('Unable to read data stream!', err);
+    }).on('close', function () {
+        console.log(`Block #${i}`);
+        addLevelDBData(i, value);
+    });
+}
+
+/* ===== Testing ==============================================================|
+|  - Self-invoking function to add blocks to chain                             |
+|  - Learn more:                                                               |
+|   https://scottiestech.info/2014/07/01/javascript-fun-looping-with-a-delay/  |
+|                                                                              |
+|  * 100 Milliseconds loop = 36,000 blocks per hour                            |
+|     (13.89 hours for 500,000 blocks)                                         |
+|    Bitcoin blockchain adds 8640 blocks per day                               |
+|     ( new block every 10 minutes )                                           |
+|  ===========================================================================*/
+
+
+function theLoop(i) {
+    setTimeout(function () {
+        addDataToLevelDB('Testing data');
+        if (--i) theLoop(i); // eslint-disable-line no-param-reassign
+    }, 100);
+}
+
+theLoop(10);
---
+++ simpleChain.js
@@ -0,0 +1,100 @@
+/* ===== SHA256 with Crypto-js ===============================
+|  Learn more: Crypto-js: https://github.com/brix/crypto-js  |
+|  =========================================================*/
+
+const SHA256 = require('crypto-js/sha256');
+
+/* ===== Block Class ==============================
+|  Class with a constructor for block             |
+|  ===============================================*/
+
+class Block {
+    constructor(data) {
+        this.hash = '';
+        this.height = 0;
+        this.body = data;
+        this.time = 0;
+        this.previousBlockHash = '';
+    }
+}
+
+/* ===== Blockchain Class ==========================
+|  Class with a constructor for new blockchain     |
+|  ================================================*/
+
+class Blockchain { // eslint-disable-line no-unused-vars
+    constructor() {
+        this.chain = [];
+        this.addBlock(new Block('First block in the chain - Genesis block'));
+    }
+
+    // Add new block
+    addBlock(newBlock) {
+        /* eslint-disable no-param-reassign */
+
+        // Block height
+        newBlock.height = this.chain.length;
+        // UTC timestamp
+        newBlock.time = new Date().getTime().toString().slice(0, -3);
+        // previous block hash
+        if (this.chain.length > 0) {
+            newBlock.previousBlockHash = this.chain[this.chain.length - 1].hash;
+        }
+        // Block hash with SHA256 using newBlock and converting to a string
+        newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+
+        /* eslint-enable no-param-reassign */
+        // Adding block object to chain
+        this.chain.push(newBlock);
+    }
+
+    // Get block height
+    getBlockHeight() {
+        return this.chain.length - 1;
+    }
+
+    // get block
+    getBlock(blockHeight) {
+        // return object as a single string
+        return JSON.parse(JSON.stringify(this.chain[blockHeight]));
+    }
+
+    // validate block
+    validateBlock(blockHeight) {
+        // get block object
+        const block = this.getBlock(blockHeight);
+        // get block hash
+        const blockHash = block.hash;
+        // remove block hash to test block integrity
+        block.hash = '';
+        // generate block hash
+        const validBlockHash = SHA256(JSON.stringify(block)).toString();
+        // Compare
+        if (blockHash === validBlockHash) {
+            return true;
+        }
+        console.log(`Block #${blockHeight} invalid hash:\n${blockHash} <> ${validBlockHash}`);
+        return false;
+    }
+
+    // Validate blockchain
+    validateChain() {
+        const errorLog = [];
+        for (let i = 0; i < this.chain.length - 1; i++) {
+            // validate block
+            if (!this.validateBlock(i))errorLog.push(i);
+            // compare blocks hash link
+            const blockHash = this.chain[i].hash;
+            const previousHash = this.chain[i + 1].previousBlockHash;
+            if (blockHash !== previousHash) {
+                errorLog.push(i);
+            }
+        }
+        if (errorLog.length > 0) {
+            console.log(`Block errors = ${errorLog.length}`);
+            console.log(`Blocks: ${errorLog}`);
+        } else {
+            console.log('No errors detected');
+        }
+    }
+}
