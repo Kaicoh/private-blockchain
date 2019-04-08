c1fcf985902f6bdf923027fa50a91d71b3a2bc7d
Kaicoh <sumireminami@gmail.com>
2019-04-08 23:46:49 +0900

    add get route

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const { Blockchain } = require('./simpleChain');
 
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+const blockchain = new Blockchain();
+
+app.get('/block/:blockHeight', async (req, res) => {
+    const blockHeight = parseInt(req.params.blockHeight, 10);
+    const block = await blockchain.getBlock(blockHeight);
+    return block ? res.json(block) : res.sendStatus(404);
 });
 
-app.listen(port, () => console.log(`Express app listening on port ${port}`));
+app.listen(port, () => {
+    console.log(`Express app listening on port ${port}`);
+});
